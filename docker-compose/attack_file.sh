e1dd45c07c7930dfae86d6e92d1f34c3ea240d57
manikjain77 <112176147+manikjain77@users.noreply.github.com>
2024-03-12 15:14:32 +0530

    Update attack_file.sh

--- docker-compose/attack_file.sh
+++ docker-compose/attack_file.sh
@@ -4,7 +4,8 @@ interval=20
 while true; do
     # Run the docker-compose command
     docker-compose -f docker-compose-ueransim-vpp.yaml up -d
-    
+    sleep 1
+    docker-compose -f docker-compose-ueransim-vpp.yaml down
     # Sleep for the specified interval
     sleep $interval
 done
